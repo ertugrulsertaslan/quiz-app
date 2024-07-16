96831ed9ecdfbfc311d9460ed80a393ebdd92bc5
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-07-16 19:16:51 +0300

    fix timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -9,11 +9,11 @@ function App() {
   const [canClick, setCanClick] = useState(false);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
         const fetchedQuestions = data.slice(0, 10).map((post) => {
-          const words = post.body.split(' ');
+          const words = post.body.split(" ");
           const randomIndices = generateRandomIndices(words.length);
           const options = randomIndices.map((index, idx) => {
             const letter = String.fromCharCode(97 + idx);
@@ -44,11 +44,6 @@ function App() {
       const timer = setInterval(() => {
         setTimeLeft((prevTime) => {
           const newTime = prevTime - 1;
-          if (newTime > 20) {
-            setCanClick(false);
-          } else {
-            setCanClick(true);
-          }
           return newTime;
         });
       }, 1000);
@@ -84,25 +79,31 @@ function App() {
   };
 
   const renderResult = () => (
-    <table className='min-w-full divide-y divide-gray-200'>
-      <thead className='bg-gray-50'>
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
         <tr>
-          <th scope='col' className='w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+          <th
+            scope="col"
+            className="w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             Question
           </th>
-          <th scope='col' className='w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+          <th
+            scope="col"
+            className="w-1/2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             Answers
           </th>
         </tr>
       </thead>
-      <tbody className='bg-white divide-y divide-gray-200'>
+      <tbody className="bg-white divide-y divide-gray-200">
         {questions.map((q, index) => (
           <tr key={index}>
-            <td className='px-6 py-4 whitespace-nowrap w-1/2'>
-              <div className='text-sm text-gray-900'>{q.question}</div>
+            <td className="px-6 py-4 whitespace-nowrap w-1/2">
+              <div className="text-sm text-gray-900">{q.question}</div>
             </td>
-            <td className='px-6 py-4 whitespace-nowrap w-1/2'>
-              <div className='text-sm text-gray-900'>{answers[index]}</div>
+            <td className="px-6 py-4 whitespace-nowrap w-1/2">
+              <div className="text-sm text-gray-900">{answers[index]}</div>
             </td>
           </tr>
         ))}
@@ -111,26 +112,26 @@ function App() {
   );
 
   return (
-    <div className='w-full items-center justify-center'>
-      <div className='w-full'>
+    <div className="w-full items-center justify-center">
+      <div className="w-full">
         <button
-          className='font-bold bg-green-500 disabled:bg-slate-400 focus:bg-green-800 text-white p-4 w-1/2 mb-10 rounded-2xl'
+          className="font-bold bg-green-500 disabled:bg-slate-400 focus:bg-green-800 text-white p-4 w-1/2 mb-10 rounded-2xl"
           onClick={ClickNext}
           disabled={!canClick}
         >
           Next Question
         </button>
       </div>
-      <div className='w-full justify-center items-center text-center flex'>
+      <div className="w-full justify-center items-center text-center flex">
         {questions.length > 0 ? (
           currentQuestion < questions.length ? (
-            <div className='w-1/2 p-5 bg-blue-600 text-white rounded-2xl'>
-              <h1 className='mb-10 font-bold text-2xl'>Question</h1>
+            <div className="w-1/2 p-5 bg-blue-600 text-white rounded-2xl">
+              <h1 className="mb-10 font-bold text-2xl">Question</h1>
               <h1>{questions[currentQuestion].question}</h1>
-              <div className='w-full  flex flex-wrap my-10 space-y-5'>
+              <div className="w-full  flex flex-wrap my-10 space-y-5">
                 {questions[currentQuestion].options.map((option) => (
                   <button
-                    className='w-full text-left p-5 border bg-red-500 disabled:bg-red-300 focus:bg-red-900'
+                    className="w-full text-left p-5 border bg-red-500 disabled:bg-red-300 focus:bg-red-900"
                     key={option}
                     onClick={() => handleAnswer(option)}
                     disabled={!canClick}
